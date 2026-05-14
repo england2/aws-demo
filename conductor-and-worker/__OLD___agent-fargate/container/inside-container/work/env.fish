d151f4792fe102ab690f68a2fb64eaf691dfd4a2
t <elanengland@protonmail.com>
2026-05-14 12:17:08 -0700

    project: deleted old reference code

--- conductor-and-worker/__OLD___agent-fargate/container/inside-container/work/env.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env fish
-
-set -gx GOROOT /root/.go
-set -gx GOPATH /root/go
-set -gx PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin $GOROOT/bin $GOPATH/bin
--- conductor-and-worker/__OLD___agent-fargate/container/inside-container/work/task.md
+++
@@ -1,3 +0,0 @@
-1. create a directory named foo
-2. create a fizzbuzz program in python
-3. run the program and check output
